e0e7a1aa7aebcafc93841868a15e1494a7581e8a
ANURAG SRIVASTAVA <anurag.25b01011260@abes.ac.in>
2026-09-25 09:42:10 +0530

    lab03

--- lab2/explab03.cpp
+++ lab2/explab03.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     float salary = 50000;
 
-    // Pointer stores address of salary
+    
     float *newSalary = &salary;
 
     *newSalary = *newSalary + (*newSalary * 10 / 100);
