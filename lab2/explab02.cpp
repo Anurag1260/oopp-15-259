65dc1306aff6f6a2c46228392e034ba6e34eb3f3
ANURAG SRIVASTAVA <anurag.25b01011260@abes.ac.in>
2026-09-25 09:41:16 +0530

    lab03

--- lab2/explab02.cpp
+++ lab2/explab02.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     float salary = 50000;
 
-    // newSalary is a reference to salary
+    
     float &newSalary = salary;
 
     newSalary = newSalary + (newSalary * 10 / 100);
